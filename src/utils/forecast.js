b2fc2cfa0923c7c12f7349bdbf3a8044eb9a3ac3
Aditi Dixit <aditidixit@Aditis-MacBook-Air.local>
2020-04-20 18:01:38 +0530

    Added humidity to forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (longitude,latitude, callback) =>{
         }else if(body.error){
             callback('Unable to provide weather forecast. Try another search. ', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity level is ' + body.current.humidity + '.')
         }
     })
 }
